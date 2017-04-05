5d46b35667d7cbb3506e2565032e10f2b85234c8
Harry Liu <harrybaa@gmail.com>
2017-04-05 11:15:34 -0700

    FFT

--- assignment4.m
+++ assignment4.m
@@ -1,21 +1,22 @@
 clear all;
-s = 16;
-fft_times = zeros(s,1);
+s = 18;
+dft_times = zeros(s,1);
 myfft_times = zeros(s,1);
 
 for i=1:s
 X = single(rand(2^i,1)) + j * single(rand(2^i,1));
     t = cputime;
-    fft(X);
-    fft_times(i) = cputime - t;
-    t = cputime;
     myfft(X);
     myfft_times(i) = cputime - t;
-fprintf( 1, 'size=%d\tmydft: %f\tfft: %f\n', 2^i, fft_times(i), myfft_times(i) );
+fprintf( 1, 'size=%d\tmydft: %f\tmyfft: %f\n', 2^i, dft_times(i), myfft_times(i) );
 end
-plot(2.^[1:s], fft_times, 'bx-' );
+
+plotX = 2.^[1:s];
+plotNLOGN = plotX.*log(plotX);
+
+plot(plotX,myfft_times, 'ro--' );
 hold;
-plot(2.^[1:s], myfft_times, 'ro--' );
-plot(2.^[1:s],((2.^[1:s]).^2) * max(mydft_times) / ((2.^s).^2),'g*:') legend('fft','myfft','nlogn');
+plot(plotX,plotNLOGN * max(myfft_times)/plotNLOGN(s),'g*:') 
+legend('myfft','MlogM');
 xlabel('M');
 ylabel('cputime');
