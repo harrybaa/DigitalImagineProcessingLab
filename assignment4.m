c601aae33b463fb0417c0769615bfcee655c81d4
Harry Liu <harrybaa@gmail.com>
2017-10-03 20:14:06 +0800

    ..

--- assignment4.m
+++ assignment4.m
@@ -1,5 +1,5 @@
 clear all;
-s = 18;
+s = 20;
 dft_times = zeros(s,1);
 myfft_times = zeros(s,1);
 
@@ -8,7 +8,7 @@ X = single(rand(2^i,1)) + j * single(rand(2^i,1));
     t = cputime;
     myfft(X);
     myfft_times(i) = cputime - t;
-fprintf( 1, 'size=%d\tmydft: %f\tmyfft: %f\n', 2^i, dft_times(i), myfft_times(i) );
+fprintf( 1, 'size=%d\tmyfft: %f\n', 2^i, myfft_times(i) );
 end
 
 plotX = 2.^[1:s];
